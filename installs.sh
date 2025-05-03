dde0818c2fc2c9da847659f05b4224de4bca0fd2
nfantaske <nf@fantasketech.com>
2025-05-03 01:56:07 -0700

    updating install script

--- installs.sh
+++ installs.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+# run from repo root dir
+
+mkdir data
+# download data automatically if possible
+
+python3 -m venv venv
+
 pip install umap-learn
 pip install pandas
 pip install h5py
