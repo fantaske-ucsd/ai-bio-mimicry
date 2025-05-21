6c65334741818966ac1f33ca7e95fd4df2712061
nfantaske <nf@fantasketech.com>
2025-05-21 21:09:08 +0000

    iterates over the matches and finds the appropriete pdb(s) to download and fills a dictionary

--- get_pdb.py
+++ get_pdb.py
@@ -6,42 +6,65 @@ from tqdm import tqdm
 csv_path = "data/tuberculosis-human-results.csv"     # Path to your CSV file
 tsv_path = "data/pdb_chain_uniprot.tsv"
 output_dir = "data/pdb_files"      # Folder to save downloaded PDBs
-pdb_columns = [0, 1]          # Index of the first two columns with PDB IDs
+
+match = {
+    'uniprot_human_id' : '',
+    'uniprot_bac_id' : '',
+    'pdb_human_ids' : [],
+    'pdb_bac_ids' : [],
+}
+
+matches = []
 
 uniprot_df = pd.read_csv(csv_path)
 pdb_df = pd.read_csv(tsv_path, sep='\t', low_memory=False).set_index("SP_PRIMARY")
 
-for i, _ in tqdm(enumerate(uniprot_df.index)) :
-    human_id = uniprot_df.at[i, 'human_id']
-    if human_id not in pdb_df.index :
+human_ids = uniprot_df['human_id'].unique()
+human_groups = uniprot_df.groupby('human_id')
+
+for human_id in tqdm(enumerate(human_ids)) :
+    if human_id[1] not in pdb_df.index :
         continue
 
+    bac_ids = human_groups.get_group(human_id[1])['bacteria_id'].unique()
+    for bac_id in enumerate(bac_ids) :
+        if bac_id[1] not in pdb_df.index :
+            continue
 
-    bac_id = uniprot_df.at[i, 'bacteria_id']
-    if bac_id not in pdb_df.index :
-        continue
+        t = pdb_df.at[human_id[1], "PDB"]
+        p = pdb_df.at[bac_id[1], "PDB"]
 
-    t = pdb_df.at[human_id, "PDB"]
-    p = pdb_df.at[bac_id, "PDB"]
+#        print(f"h: {human_id} b: {bac_id}")
 
-    print(f"h: {human_id} b: {bac_id} cycle {i}")
+        if not isinstance(t, str):
+            tu = t.dropna().unique()
+        else:
+            tu = []
+            tu.append(t)
+#        print(f"Unique human len {len(tu)}")
+#        print(tu)
 
-    print(f"\nHuman len {len(t)}")
-    print(type(t))
+        if not isinstance(p, str):
+            pu = p.dropna().unique()
+        else:
+            pu = []
+            pu.append(p)
+#        print(f"Unique pathogen len {len(pu)}")
+#        print(pu)
 
-    print(f"\nPathogen len {len(p)}")
-    print(type(p))
+        new_match = dict(match)
+        new_match['uniprot_human_id'] = human_id[1]
+        new_match['uniprot_bac_id'] = bac_id[1]
+        new_match['pdb_human_ids'] = list(tu)
+        new_match['pdb_bac_ids'] = list(pu)
+        matches.append(new_match)
 
-    break
+print(f"Found {len(matches)} matches")
 
-#t = pdb_df.at[bac_id, "PDB"]
+#print(t.iloc[0])
+#print(p.iloc[0])
 
 """
-# === EXTRACT UNIQUE PDB IDs ===
-pdb_ids = set()
-for col in pdb_columns:
-    pdb_ids.update(df.iloc[:, col].dropna().astype(str).str.strip().str.lower())
-
 # === DOWNLOAD EACH PDB FILE ===
 for pdb_id in pdb_ids:
     if len(pdb_id) != 4:
