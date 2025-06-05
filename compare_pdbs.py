93fa0b5ff335c503963d0ea43d74cd6b724f51d8
nfantaske <nfantaske@ucsd.edu>
2025-06-05 22:54:57 +0000

    added compare.py script file, WIP

---
+++ compare_pdbs.py
@@ -0,0 +1,12 @@
+from bio_functions import *
+
+match = {
+    'uniprot_human_id' : '',
+    'uniprot_bac_id' : '',
+    'pdb_human_ids' : [],
+    'pdb_bac_ids' : [],
+    "human_pdb_exists": True,
+    "bac_pdb_exists": True,
+}
+
+
